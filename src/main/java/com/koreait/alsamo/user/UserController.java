c46d8884909bf863086c39e5c6cdcf7e3beb628f
heck_evil <heck_evil@naver.com>
2021-06-11 20:07:48 +0900

    2021 06 11 MASTER SETTING

---
+++ src/main/java/com/koreait/alsamo/user/UserController.java
@@ -0,0 +1,13 @@
+package com.koreait.alsamo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("login")
+    public String login() {
+        return "user/login";
+    }
+}
