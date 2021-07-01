e3c6effe74e8e3b58166c1e9727275b78ad37c26
jincheol2578 <jincheol2578@gmail.com>
2021-07-01 15:42:31 +0900

    21.06.30 pagination 마무리, ckeditor 적용, ckeditor 이미지 업로드 중

--- src/main/java/com/koreait/alsamo/utils/MyUtils.java
+++ src/main/java/com/koreait/alsamo/utils/MyUtils.java
@@ -1,10 +1,15 @@
 package com.koreait.alsamo.utils;
 
+import com.koreait.alsamo.mailsender.MailHandler;
 import com.koreait.alsamo.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+
 @Component
 public class MyUtils {
 // TODO: 가능하면 static으로 바꾸기
@@ -23,5 +28,13 @@ public class MyUtils {
     // Session에서 가져온 User객체의 uno 리턴해주는 메소드
     public int getUserPk(){
         return getLoginUser() == null ? 0 : getLoginUser().getUno();
+    }
+    public void mailSender(String to, String subject, String txt) throws MessagingException, UnsupportedEncodingException {
+        MailHandler mailHandler = new MailHandler(javaMailSender);
+        mailHandler.setSubject(subject);
+        mailHandler.setText(txt);
+        mailHandler.setFrom("heckevil12@gmail.com", "alsamo");
+        mailHandler.setTo(to);
+        mailHandler.send();
     }
 }
